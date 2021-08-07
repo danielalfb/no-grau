9e06211e43ea340bf851e200c3027ab6e49c369f
inglyd <inglydalves@outlook.com>
2021-08-07 02:27:27 -0300

    criando o componente Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Nav, Menu, NavLink, Bars, Logo } from './style';
+import logo from '../../images/no-grau-logo.svg';
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Nav>
+        <Logo>
+          <img src={logo} alt="logo" />
+        </Logo>
+        <Bars onClick={() => setIsOpen(!isOpen)} />
+        <Menu isOpen={isOpen}>
+          <NavLink to="/home" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/quem-somos" activeStyle>
+            Quem somos
+          </NavLink>
+          <NavLink to="/vitrine" activeStyle>
+            Vitrine
+          </NavLink>
+          <NavLink to="/filtro" activeStyle>
+            Filtro
+          </NavLink>
+        </Menu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+// ajustar o caminho da pasta de foto com a pasta de images do repo
