8f28acf78d6e8cf5941d012555d80758e09e02b6
danielalfb <danielalfbarbosa@gmail.com>
2021-08-10 16:26:23 -0300

    responsividade dos títulso e subtítulos

--- src/components/TitleSubtitle/style.js
+++ src/components/TitleSubtitle/style.js
@@ -8,9 +8,15 @@ export const Box = styled.div`
   margin: 10px 0;
   font-family: 'Roboto', sans-serif;
   font-weight: 500;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding: 0 30px;
+  }
 `;
 
 export const Title = styled.h1`
+  max-width: 100%;
   font-size: 26px;
   line-height: 58px;
   letter-spacing: -0.9px;
@@ -20,6 +26,7 @@ export const Title = styled.h1`
 
 export const Subtitle = styled.p`
   width: 500px;
+  max-width: 100%;
   font-size: 18px;
   line-height: 28px;
   letter-spacing: 0.1px;
