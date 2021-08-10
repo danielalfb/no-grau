3452a6639fe16a3797ffbf22c63828b7dd8682f5
Thais <thaisferraznogueira@gmail.com>
2021-08-10 15:10:26 -0300

    Sobre

---
+++ src/components/Sobre/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Sobre } from './style';
+import sobre from '../../assets/images/sobre.svg';
+
+const Sobre = () => {
+  return (
+    <Sobre>
+      <img src={sobre} alt="sobre" />
+    </Sobre>
+  );
+};
+
+export default Sobre;
---
+++ src/components/Sobre/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Sobre = styled.div`
+img {
+display: block; 
+margin-left: auto; 
+margin-right: auto;
+}
+
+@media screen and (max-width: 768px) {
+  display: none;
+  }
+`;
